b99d52a39252a566a97c7d7eae6e79e8d1f63425
Omar ELRayes <omar.elrayes@cassbana.com>
2023-01-02 17:23:31 +0200

    Base scanning functionality.

--- app/src/main/java/com/cassbana/demo/MainActivity.kt
+++ app/src/main/java/com/cassbana/demo/MainActivity.kt
@@ -2,10 +2,61 @@ package com.cassbana.demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.cassbana.demo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        startCamera()
+
+        viewBinding.button.setOnClickListener {
+            startCamera()
+        }
+    }
+
+    private fun startCamera() {
+        viewBinding.scanner.start(
+            this@MainActivity,
+            onBarcodeDetected = {
+                Toast.makeText(this, "Detected $it", Toast.LENGTH_SHORT).show()
+            }, onPermissionNeeded = {
+                ActivityCompat.requestPermissions(
+                    this@MainActivity,
+                    it,
+                    REQUEST_CODE_PERMISSIONS
+                )
+            })
+    }
+
+    companion object {
+        private const val REQUEST_CODE_PERMISSIONS = 10
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults:
+        IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (grantResults.isNotEmpty()) {
+                startCamera()
+            } else {
+                Toast.makeText(
+                    this,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
     }
 }
